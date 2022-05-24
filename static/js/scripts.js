9f1be459664087721f727672949287e9f564b6cd
Alex Moreira <amoreira@N102.annex>
2022-05-24 15:59:25 -0300

    Adicionando arquivos js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -129,7 +129,7 @@ if (gridCheck !== null) {
 
 /* Back To Top Button */
 // Get the button
-var myButton = document.getElementById("myBtn");
+myButton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 function scrollFunctionBTT() {
