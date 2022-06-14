7eb10869d36902e6ddf949540c347d1e123f0730
Alex Moreira <amoreira@annex>
2022-06-14 12:14:55 -0300

    Configurando arquivos media em Deploy v2

--- projeto/settings.py
+++ projeto/settings.py
@@ -130,6 +130,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+MEDIA_URL = ''
+MEDIA_ROOT = BASE_DIR / 'static/media'
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
@@ -138,8 +141,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 WHITENOISE_AUTOREFRESH = True
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
