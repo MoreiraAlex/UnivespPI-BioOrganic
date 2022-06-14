ff228894d519189af09ea5669dbf6949e5a192a5
Alex Moreira <amoreira@annex>
2022-06-14 12:05:46 -0300

    Configurando arquivos media em Deploy

--- projeto/settings.py
+++ projeto/settings.py
@@ -136,6 +136,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_AUTOREFRESH = True
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
