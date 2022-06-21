ce3dc32629ebcff2d4a19f4cafaeed0142f83b42
Alex Moreira <amoreira@annex>
2022-06-21 17:23:57 -0300

    Consentimento de Cookies

--- bio/urls.py
+++ bio/urls.py
@@ -5,5 +5,6 @@ app_name = 'bio'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('privacy', views.privacy, name='privacy'),
     path('manun', views.manun, name='manutenção'),
 ]
--- bio/views.py
+++ bio/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+def privacy(request):
+    return render(request, 'privacy.html')
+
 def manun(request):
     return render(request, 'manutencao.html')
