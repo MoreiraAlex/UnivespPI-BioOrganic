bf2626a53534eb686e171af030449a0dbd0c4fab
Alex Moreira <amoreira@annex>
2022-06-21 15:34:54 -0300

    Amazon s3

--- projeto/heroku.py
+++ projeto/heroku.py
@@ -14,3 +14,14 @@ DATABASES = {
 }
 
 SITE_ID = env.int('SITE_ID', 1)
+
+#AMAZOM S3
+#################################################################################
+AWS_ACCESS_KEY_ID = 'AKIAVMZ77YHHMR6DZTWR'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'biorganic'
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#################################################################################
